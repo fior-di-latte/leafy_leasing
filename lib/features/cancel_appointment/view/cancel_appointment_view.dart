9ea1ab7f16cddddc56c442ce694f4df6300bea84
FELIX PLAGGE <felix.plagge@gmail.com>
2023-04-28 16:14:48 +0200

    REF

--- lib/features/cancel_appointment/view/cancel_appointment_view.dart
+++ lib/features/cancel_appointment/view/cancel_appointment_view.dart
@@ -13,83 +13,88 @@ class CancelAppointmentScreen extends HookConsumerWidget with UiLoggy {
     final hasChosenAlready = canceledByCustomer.value != null;
     final commentTextController = useTextEditingController();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(ctx.lc.cancelAppointmentTitle),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(lPadding),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 7,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    AnimatedPositioned(
-                      duration: 450.milliseconds,
-                      curve: Curves.easeOut,
-                      top: hasChosenAlready ? 0 : 200,
-                      child: WhoCanceledRadioButtons(
-                          canceledByCustomer: canceledByCustomer,),
+      appBar: AppBar(
+        title: Text(ctx.lc.cancelAppointmentTitle),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(lPadding),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 7,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  AnimatedPositioned(
+                    duration: 450.milliseconds,
+                    curve: Curves.easeOut,
+                    top: hasChosenAlready ? 0 : 200,
+                    child: WhoCanceledRadioButtons(
+                      canceledByCustomer: canceledByCustomer,
                     ),
-                    Positioned.fill(
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.symmetric(vertical: lPadding),
-                          child: AnimatedSwitcher(
-                            switchInCurve: Curves.easeIn,
-                            duration: 260.milliseconds,
-                            child: hasChosenAlready
-                                ? const SizedBox.shrink()
-                                : Text(
-                                    'Who canceled\nthe appointment?',
-                                    style: ctx.tt.headlineLarge!.copyWith(
-                                        color: ctx.cs.onBackground,
-                                        fontStyle: FontStyle.italic,),
+                  ),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: lPadding),
+                        child: AnimatedSwitcher(
+                          switchInCurve: Curves.easeIn,
+                          duration: 260.milliseconds,
+                          child: hasChosenAlready
+                              ? const SizedBox.shrink()
+                              : Text(
+                                  'Who canceled\nthe appointment?',
+                                  style: ctx.tt.headlineLarge!.copyWith(
+                                    color: ctx.cs.onBackground,
+                                    fontStyle: FontStyle.italic,
                                   ),
-                          ),
+                                ),
                         ),
                       ),
                     ),
-                    Positioned.fill(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.symmetric(vertical: lPadding),
-                          child: AnimatedSwitcher(
-                            switchInCurve: Curves.easeIn,
-                            duration: 260.milliseconds,
-                            child: hasChosenAlready
-                                ?
-                                // Textfield with input decoration and caption: 'Comment (optional')
-                                CommentTextField(commentTextController)
-                                : const SizedBox.shrink(),
-                          ),
+                  ),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: lPadding),
+                        child: AnimatedSwitcher(
+                          switchInCurve: Curves.easeIn,
+                          duration: 260.milliseconds,
+                          child: hasChosenAlready
+                              ?
+                              // Textfield with input decoration and caption: 'Comment (optional')
+                              CommentTextField(commentTextController)
+                              : const SizedBox.shrink(),
                         ),
                       ),
-                    )
-                  ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Gap(mPadding),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Hero(
+                  tag: kCancelButtonHeroTag,
+                  child: AppointmentActionButton.cancelFinalize(
+                    ctx,
+                    ref,
+                    commentTextController: commentTextController,
+                    newStatus: _getNewStatus(canceledByCustomer.value),
+                    isGreyedOut: !hasChosenAlready,
+                    id: id,
+                  ),
                 ),
               ),
-              const Gap(mPadding),
-              Expanded(
-                  flex: 2,
-                  child: Center(
-                      child: Hero(
-                          tag: kCancelButtonHeroTag,
-                          child: AppointmentActionButton.cancelFinalize(
-                              ctx, ref,
-                              commentTextController: commentTextController,
-                              newStatus:
-                                  _getNewStatus(canceledByCustomer.value),
-                              isGreyedOut: !hasChosenAlready,
-                              id: id,),),),)
-            ],
-          ),
-        ),);
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   AppointmentStatus? _getNewStatus(bool? canceledByCustomer) {
