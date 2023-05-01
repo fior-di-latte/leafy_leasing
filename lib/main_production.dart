7880d8b8c89c6cc79aa6c3621d646318d96bc700
FELIX PLAGGE <felix.plagge@gmail.com>
2023-05-01 12:00:42 +0200

    DEP: pre-commit hook

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,7 +2,7 @@
 import 'package:leafy_leasing/shared/base.dart';
 
 Future<void> main() async {
-  final productionEnv = DotEnv();
+  DotEnv productionEnv = DotEnv();
   await productionEnv.load(fileName: Assets.dotenvProduction);
   await dotenv.load(
     fileName: Assets.dotenvGlobal,
