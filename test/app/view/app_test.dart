d90b29bbd1895e2fa3573a38843d33e85ce509a4
FELIX PLAGGE <felix.plagge@gmail.com>
2023-05-02 23:23:18 +0200

    DEP: pre-commit hook

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leafy_leasing/app/app.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(Container(color: Colors.red));
-      print('rofl');
-      expect(find.byType(AppBar), findsOneWidget);
+      expect(find.byType(Container), findsOneWidget);
     });
   });
 }
