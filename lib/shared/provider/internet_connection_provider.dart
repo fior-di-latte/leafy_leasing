7122a78eaba37cd45a8910ed57fca99c86049f81
FELIX PLAGGE <felix.plagge@gmail.com>
2023-05-09 23:44:09 +0200

    BFX: internet subscription stuff

--- lib/shared/provider/internet_connection_provider.dart
+++ lib/shared/provider/internet_connection_provider.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:leafy_leasing/shared/base.dart';
 
 part 'internet_connection_provider.g.dart';
 
 @riverpod
-InternetConnectionChecker internetConnection(InternetConnectionRef ref) =>
-    getInternetConnectionChecker();
+StreamSubscription<InternetConnectionStatus> internetConnection(
+    InternetConnectionRef ref) {
+  final streamSubscription = getInternetCheckStream();
+  ref.onDispose(streamSubscription.cancel);
+  return streamSubscription;
+}
