f2bd34000ee10d5332ef95a26a85c9a458e5c83f
FELIX PLAGGE <felix.plagge@gmail.com>
2023-06-01 00:08:30 +0200

    WIP cache

--- lib/shared/provider/provider.dart
+++ lib/shared/provider/provider.dart
@@ -13,33 +13,40 @@ export 'settings_provider.dart';
 
 enum FetchingStrategy { single, polling, stream }
 
-enum CacheReadStrategy { fallback, first, no }
+// fallback: if internet connection is not available, search in cache, if not found, retry to fetch
+//           if internet connection is available, fetch, if fails, try cache, if fails retry fetch
+// eagerCacheWhileLoading: regardless of internet connection, initialize data from cache first, then fetch
+// no: no cache at all is used.
+enum CacheReadStrategy { fallback, eager, no }
 
 typedef ErrorUiCallback = void Function(BuildContext context);
 
 mixin AsyncProviderMixin<T, ID> {
   @protected
   late Repository<T, ID> repository;
-  late Cache<T> cache;
+  late Cache<T> _cache;
+  late CacheReadStrategy _cacheRead;
   // this should be identical to the parameter 'id' from the build method,
   // if it exists
   late ID _id;
   // this can be final as _fromPolling is called only once after
-  // the cachedRepository is initialized
+  // the cachedRepository is initialized (that triggers a rebuild
   late final Timer _pollingTimer;
 
   abstract AsyncValue<T> state;
   AutoDisposeAsyncNotifierProviderRef<T> get ref;
 
-  Future<void> _initialize(
+  Future<void> _initializeFields(
     ID id,
     AutoDisposeFutureProvider<(Repository<T, ID>, Cache<T>)>
         cachedRepositoryProvider,
+    CacheReadStrategy cacheRead,
   ) async {
     _id = id;
+    _cacheRead = cacheRead;
     final cachedRepository = await ref.watch(cachedRepositoryProvider.future);
     repository = cachedRepository.$1;
-    cache = cachedRepository.$2;
+    _cache = cachedRepository.$2;
   }
 
   @protected
@@ -67,11 +74,13 @@ mixin AsyncProviderMixin<T, ID> {
     required ID id,
     required FetchingStrategy strategy,
     Duration pollingInterval = const Duration(milliseconds: 5000),
+    CacheReadStrategy cacheRead = CacheReadStrategy.fallback,
   }) async {
     _mount();
     _onDispose();
 
-    await _initialize(id, cachedRepositoryProvider);
+    await _initializeFields(id, cachedRepositoryProvider, cacheRead);
+
     return switch (strategy) {
       (FetchingStrategy.single) => _fromGet(),
       (FetchingStrategy.stream) => _fromStream(),
@@ -139,15 +148,60 @@ mixin AsyncProviderMixin<T, ID> {
         retryIf: (e) => e is SocketException || e is TimeoutException,
         onRetry: (e) => logOnNetworkRetry<T>(loggingKey, e, isPut: isPut),
       );
-  Future<T> get(ID id) =>
-      retryAndLog(() => repository.get(id), loggingKey: id.toString());
+
+  Future<T> _get(ID id) => switch (_cacheRead) {
+        (CacheReadStrategy.fallback) => _getWithFallback(id),
+        (CacheReadStrategy.eager) => _getWithEager(id),
+        (CacheReadStrategy.no) => _getWithNoCache(id),
+      };
+
+  // Future<T> get(ID id) =>
+  //     retryAndLog(() => repository.get(id), loggingKey: id.toString());
+
+  Future<T> _getWithFallback(ID id) {
+    throw UnimplementedError();
+  }
+
+  Future<T> _getWithEager(ID id) {
+    throw UnimplementedError();
+  }
+
+  Future<T> _getWithNoCache(ID id) {
+    throw UnimplementedError();
+  }
+
+  // Future<T> rofl([String? id]) => asyncWrapper(
+  //       () async {
+  //         print('haha');
+  //         try {
+  //           final incomingValue = await getter(id);
+  //           // update cache
+  //           await _cache.put(id ?? 'globalKey', incomingValue);
+  //           return incomingValue;
+  //         } catch (e) {
+  //           // check if cache fallback is available
+  //           final cachedFallback = await _cache.get(id ?? 'globalKey');
+  //           if (cachedFallback != null) {
+  //             logger.i(
+  //               '😅 Cache Fallback Used! Found $id in cache, returning $cachedFallback',
+  //             );
+  //             return cachedFallback;
+  //           }
+  //
+  //           // no incoming value && no cache fallback -> rethrow Error
+  //           logger.w('No incoming value and no cache fallback for $id');
+  //           rethrow;
+  //         }
+  //       },
+  //       loggingKey: id,
+  //     );
 
   Future<T> put(T item, {required ID id}) => retryAndLog(
         () => repository.put(item, id: id),
         loggingKey: item.toString(),
       );
 
-  Future<T> _fromGet() => get(_id);
+  Future<T> _fromGet() => _get(_id);
 
   // 'Mounted' mixin, as in riverpod issue 1879
   Object? _initial;
