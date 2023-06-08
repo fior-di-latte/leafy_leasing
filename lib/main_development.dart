fbc68b74abc470f99ca755d47639c4dce7b57188
FELIX PLAGGE <felix.plagge@gmail.com>
2023-06-08 13:26:26 +0200

    REF

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,11 @@
 // Project imports:
+import 'dart:async';
+
 import 'package:leafy_leasing/shared/base.dart';
 
 Future<void> main() async {
   final devEnv = DotEnv();
   await devEnv.load(fileName: Assets.dotenvDev);
   await dotenv.load(fileName: Assets.dotenvGlobal, mergeWith: devEnv.env);
-  await bootstrap();
+  unawaited(bootstrap());
 }
